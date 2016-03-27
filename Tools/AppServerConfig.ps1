e939000d672c991fee62b4abd53518f905fa4750
ChaseManhatton <email@chasemanhatton.com>
2016-03-27 01:16:42 -0500

    DSC Script updates.

--- Tools/AppServerConfig.ps1
+++ Tools/AppServerConfig.ps1
@@ -23,5 +23,18 @@ Configuration IISConfig
         Name = "Web-Mgmt-Console"
         Ensure = "Present"
     }
+
+    File CreateFile {
+        DestinationPath = 'C:\inetpub\wwwroot\serverid.txt'
+        Ensure = "Present"
+        Contents = $MachineName
+    }
+
+    Log AfterDirectoryCopy
+    {
+        # The message below gets written to the Microsoft-Windows-Desired State Configuration/Analytic log
+        Message = "Finished running the file resource with ID CreateFile"
+        DependsOn = "[File]CreateFile" # This means run "CreateFile" first.
+    }
   }
-} 
+}
